2d60c673ce68c35c9708c6231c11df209ddba36f
NikitaUnkind <ratajczakk@mail.ru>
2017-12-12 16:44:54 +0300

    emerald 2-th commit

--- emerald/js/common.js
+++ emerald/js/common.js
@@ -1,22 +1,21 @@
-$(function() {
 
 	$(document).ready(function () {
-        $('.menu-icon').click(function () {
-            $('.header-menu').css('right','0px');
+        $('.menu-icon , .text-container').click(function () {
+            $('.menu-icon').animate({right:'-100'},100);
+            $('.text-container').animate({right:'-100'},350);
+            $('.header-menu').css('-webkit-clip-path','polygon(0 0,100% 0,100% 100%,0 100%)');
             function showMenu() {
-                $('.header-menu').css('-webkit-clip-path','polygon(0 0,100% 0,100% 100%,0 100%)');
-                $('.menu-icon').animate({right:'-100'},300);
+                $('.header-menu').css('right','0px');
             }
-            setTimeout(showMenu,100);
+            setTimeout(showMenu,500);
         });
         $('.menu-close').click(function () {
             $('.header-menu').css('-webkit-clip-path','polygon(0 0,100% 0,100% 100%,0 100%)');
             function hideMenu() {
                 $('.header-menu').css('right','-100vw');
-                $('.menu-icon').animate({right:'50'},300);
+                $('.menu-icon').animate({right:'50'},1000);
+                $('.text-container').animate({right:'112'},750);
             }
             setTimeout(hideMenu,100);
         });
     });
-
-});
