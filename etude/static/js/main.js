d321e5b22ecf28df63c1efd457c1c03fef26d76c
NikitaUnkind <lliamet@mail.ru>
2018-05-13 20:38:50 +0300

    etude first commit

---
+++ etude/static/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $('.js-index-slider').slick({
+        arrows: false,
+        prevArrow: '.index-slider__controls-prev',
+        nextArrow: '.index-slider__controls-next',
+        dots: true,
+        customPaging: function () {
+            return '<a class="index-slider__dots"></a>';
+        },
+    });
+    function indexSliderElemPos(elem, pos) {
+        var windowWidth = $(window).width(),
+            containerWidth = $('.container').width(),
+            leftPos = (windowWidth-containerWidth)/2;
+        $('.index-slider ' + elem).css(pos,leftPos);
+    }
+    indexSliderElemPos('.slick-dots','left');
+    indexSliderElemPos('.index-slider__controls','right');
+
+    $(window).resize(function () {
+        indexSliderElemPos('.slick-dots','left');
+        indexSliderElemPos('.index-slider__controls','right');
+    })
+});
