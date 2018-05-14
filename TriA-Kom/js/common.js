ae2afd5b433d83e32d62a122b1659fbdbcdf601a
NikitaUnkind <lliamet@mail.ru>
2018-05-14 23:53:26 +0300

    second triacom commit

--- TriA-Kom/js/common.js
+++ TriA-Kom/js/common.js
@@ -1,5 +1,18 @@
-$(function() {
-
-	// Custom JS
-
+$(document).ready(function(){
+    $('.carousel-team').owlCarousel({
+        loop: true,
+        responsiveClass: true,
+        smartSpeed: 1500,
+        responsive: {
+            0: {
+                items: 1
+            },
+            800: {
+                items: 2
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
 });
